d881cb6eb48eb50ba30a391cdbb51468236a9ada
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-12-09 00:12:27 +0000

    Add beginning of prob 2 solution, still not very close to it

--- first-project/src/main.cpp
+++ first-project/src/main.cpp
@@ -9,9 +9,9 @@ typedef std::vector<ll> Pile;
 typedef ll Card; 
 
 ll parseVector(std::vector<Card> &v);
-void prllResults(ll longest, ll amount);
+void printResults(ll longest, ll amount);
 void solveLIS(std::vector<Card> &v, ll noElements); // LIS standing for Longest Increasing Subsequence
-void solveLCIS(); // LCIS standing for Longest Common Increasing Subsequence
+void solveLCIS(std::vector<ll> &v1, std::vector<ll> &v2, int noElements1, int noElements2); // LCIS standing for Longest Common Increasing Subsequence
 std::vector<ll> generateVector(ll n);
 
 int main() {
@@ -24,7 +24,11 @@ int main() {
     int noElements = parseVector(v);
     solveLIS(v, noElements);
   } else {
-    solveLCIS();
+    ll noElements1, noElements2;
+    std::vector<ll> v1, v2;
+    noElements1 = parseVector(v1);
+    noElements2 = parseVector(v2);
+    solveLCIS(v1, v2, noElements1, noElements2);
   }
   
   return 0;
@@ -105,8 +109,35 @@ void solveLIS(std::vector<ll> &v, ll noElements) { // solve the longest increasi
     ) - piles.begin();
     pileIndex == 0 ? addLeftmostPile(piles, cumSums, card) : addOtherPile(piles, cumSums, pileIndex, card);
   }
-
+ 
   printResults(piles.size(), cumSums.back().back());
 }
 
-void solveLCIS() { } // TODO: solve the longest common increasing subsequence problem using patience sorting
+void solveLCIS(std::vector<ll> &v1, std::vector<ll> &v2, int noElements1, int noElements2) {
+  // each table element will be the max LCS ending in that index's element in v2
+  // if an element is not in v2, it will be 0 in the table!
+  // TODO - still doesnt work for vectors with repeated elements (i.e 1 2 3 and 1 2 2 3 2)
+  std::vector<ll> table = std::vector<ll>(noElements2, 0);
+  std::unordered_map<ll, ll> amounts; // TODO is it access O(1) or O(n)?
+
+  for (ll i = 0; i < noElements1; i++) {
+    ll currMax = 0;
+    for (ll j = 0; j < noElements2; j++) {
+      if (v1[i] > v2[j]) {
+        currMax = std::max(table[j], currMax); 
+      } else if (v1[i] == v2[j]) {
+        ll newMax = std::max(table[j], currMax + 1);
+        table[j] = newMax;
+        amounts[newMax]++;
+      }
+    }
+  }
+
+  for (ll num: table) {
+    std::cout << num << " ";
+  }
+  std::cout << std::endl;
+
+  ll maxElement = *std::max_element(table.begin(), table.end());
+  printResults(maxElement, amounts[maxElement]);
+}
