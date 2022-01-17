23f5fdd8be03a124606a363d3e4edf1e724c9b0e
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-01-17 17:29:41 +0000

    Further optimized, 1200/1700, WHITE for GRAY

--- second-project/src/main.cpp
+++ second-project/src/main.cpp
@@ -14,13 +14,10 @@ typedef enum { WHITE, GRAY, BLACK } Color;
 typedef std::unordered_map<Node, std::vector<Node>> Graph;
 typedef std::unordered_map<Node, Color> Ancestors;
 
-bool isCyclicUtil(Node v, std::vector<Color> &visited, Graph &adjList) {
+bool isCyclicVisit(Node &v, std::vector<Color> &visited, Graph &adjList) {
   visited[v] = GRAY;
   for (Node u: adjList[v]) {
-    if (
-      visited[u] == GRAY ||
-      (visited[u] == WHITE && isCyclicUtil(u, visited, adjList))  
-    ) {
+    if (visited[u] == GRAY || (visited[u] == WHITE && isCyclicVisit(u, visited, adjList))) {
       return true;
     }
   }
@@ -28,81 +25,83 @@ bool isCyclicUtil(Node v, std::vector<Color> &visited, Graph &adjList) {
   return false;
 }
 
-bool isCyclic(int noNodes, Graph &adjList) {
+bool isCyclic(int &noNodes, Graph &adjList) {
   std::vector<Color> visited(noNodes, WHITE);
   for (Node v: visited) {
-    if (v == WHITE) {
-      if (isCyclicUtil(v, visited, adjList)) {
-        return true;
-      }
+    if (v == WHITE && isCyclicVisit(v, visited, adjList)) {
+      return true;
     }
   }
   return false;
 }
 
-void xLookup(Node x, Graph &parents, Ancestors &xTable) {
+void xLookup(Node &x, Graph &parents, Ancestors &xTable) {
   xTable[x] = BLACK;
   for (Node xParent : parents[x]) {
     if(xTable[xParent] != BLACK) xLookup(xParent, parents, xTable);
   }
 }
 
-void yCleanup(Node y, Graph &parents, Ancestors &yTable) {
+void yCleanup(Node &y, Graph &parents, Ancestors &yTable) {
   yTable[y] = BLACK;
   for (Node yParent : parents[y]) {
     if (yTable[yParent] != BLACK) yCleanup(yParent, parents, yTable);
   }
 }
 
-void yLookup(Node y, Graph &parents, Ancestors &xTable, Ancestors &yTable) {
-  if (xTable[y] == BLACK && yTable[y] != BLACK) {
-    yTable[y] = WHITE;
+void yLookup(Node &y, Graph &parents, Ancestors &xTable, Ancestors &yTable) {
+  if (xTable[y] == BLACK && yTable[y] == WHITE) {
+    yTable[y] = GRAY;
     for (Node yParent : parents[y]) {
-      if (yTable[y] != BLACK) yCleanup(yParent, parents, yTable);
+      if (yTable[yParent] != BLACK) yCleanup(yParent, parents, yTable); // IT NEEDS TO BE GRAY!
     }
   } else {
+    yTable[y] = BLACK;
     for (Node yParent : parents[y]) {
-      yLookup(yParent, parents, xTable, yTable);
+      if (yTable[yParent] != GRAY) yLookup(yParent, parents, xTable, yTable);
     }
   }
 }
 
-void lca(Node x, Node y, Graph &parents) {
+void lca(Node &x, Node &y, Graph &parents) {
   Ancestors xTable;
   Ancestors yTable;
   xLookup(x, parents, xTable);
-  yLookup(y, parents, xTable, yTable);
-  // sort yTable's keys that are WHITE
-  std::vector<Node> whiteKeys;
+  yLookup(y, parents, xTable, yTable); // they all start at WHITE
+  // sort yTable's keys that are GRAY
+  std::vector<Node> grayKeys;
   for (auto pair: yTable) {
-    if (pair.second == WHITE) whiteKeys.push_back(pair.first);
+    if (pair.second == GRAY) grayKeys.push_back(pair.first);
   }
-  std::sort(whiteKeys.begin(), whiteKeys.end());
-  for (Node v: whiteKeys) {
+  std::sort(grayKeys.begin(), grayKeys.end());
+  for (Node v: grayKeys) {
     std::cout << v << " ";
   }
-  if (whiteKeys.size() == 0) {
+  if (grayKeys.size() == 0) {
     std::cout << "-";
   }
   std::cout << std::endl;
 }
 
 int main() {
+  std::ios_base::sync_with_stdio(false);
   int n, m;
-  scanf("%d %d", &n, &m);
-  getchar();
+  std::cin >> n >> m;
   int noNodes, noEdges;
-  scanf("%d %d", &noNodes, &noEdges);
-  
+  std::cin >> noNodes >> noEdges;
+
   // parents[i] = [u, v] means that u and v are i's parents
   Graph parents;
+  // reserve space for all nodes
+  parents.reserve(noNodes);
   // adjList[i] = [j, k] means that there is an edge from i to j and from i to k
   Graph adjList;
+  // reserve space for all nodes
+  adjList.reserve(noNodes);
 
   for (int i = 0; i < noEdges; i++) {
-    getchar();
     int x, y;
-    scanf("%d %d", &x, &y);
+    std::cin >> x >> y;
     if (parents[y].size() == 2) { // we would be adding a third parent
       std::cout << 0 << std::endl;
       return 0;
