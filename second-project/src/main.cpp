744e68f38bff0d02737d11b87f0cc9a3a643207d
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-01-18 09:44:11 +0000

    1700/1700

--- second-project/src/main.cpp
+++ second-project/src/main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <algorithm>
 
+#define MAX_PARENTS 2
 using Node = int;
 using Color = enum { WHITE, GRAY, BLACK };
 using Graph = std::vector<std::vector<Node>>;
@@ -52,21 +53,21 @@ void yLookup(Node &y, Graph &parents, Ancestors &xTable, Ancestors &yTable) {
   if (xTable[y] == BLACK && yTable[y] == WHITE) {
     yTable[y] = GRAY;
     for (Node yParent : parents[y]) {
-      if (yTable[yParent] != BLACK) yCleanup(yParent, parents, yTable); // CLEANED IF IT HASN'T ALREADY BEEN
+      if (yTable[yParent] != BLACK) yCleanup(yParent, parents, yTable); // cleaned if it hasn't already been
     }
   } else {
     yTable[y] = BLACK;
     for (Node yParent : parents[y]) {
-      if (yTable[yParent] != GRAY) yLookup(yParent, parents, xTable, yTable);
+      if (yTable[yParent] == WHITE) yLookup(yParent, parents, xTable, yTable);
     }
   }
 }
 
-void lca(Node &x, Node &y, Graph &parents) {
-  Ancestors xTable(parents.size() + 1, WHITE);
-  Ancestors yTable(parents.size() + 1, WHITE);
+void lca(int &noNodes, Node &x, Node &y, Graph &parents) {
+  Ancestors xTable(noNodes + 1, WHITE); // they all start at WHITE
+  Ancestors yTable(noNodes + 1, WHITE); // they all start at WHITE
   xLookup(x, parents, xTable);
-  yLookup(y, parents, xTable, yTable); // they all start at WHITE
+  yLookup(y, parents, xTable, yTable);
   // sort yTable's keys that are GRAY
   std::vector<Node> grayKeys;
   for (Node i = 0; i < (int) yTable.size(); i++) {
@@ -84,6 +85,7 @@ void lca(Node &x, Node &y, Graph &parents) {
 
 int main() {
   std::ios_base::sync_with_stdio(false);
+
   int n, m;
   std::cin >> n >> m;
   int noNodes, noEdges;
@@ -91,24 +93,21 @@ int main() {
 
   // parents[i] = [u, v] means that u and v are i's parents
   Graph parents(noNodes + 1);
-  // adjList[i] = [j, k] means that there is an edge from i to j and from i to k
-  Graph adjList(noNodes + 1);
 
-  int x, y;
-  for (int i = 0; i < noEdges && std::cin >> x >> y; i++) {
-    if (parents[y].size() == 2) { // we would be adding a third parent
+  Node x, y;
+  int readEdges;
+  for (readEdges = 0; readEdges < noEdges && std::cin >> x >> y; readEdges++) {
+    if (parents[y].size() == MAX_PARENTS) {
       std::cout << 0 << std::endl;
       return 0;
     }
     parents[y].push_back(x); // x is y's parent
-    adjList[x].push_back(y); // y is x's child
   }
 
-  if (isCyclic(noNodes, adjList)) {
+  if (readEdges < noEdges || isCyclic(noNodes, parents)) {
     std::cout << 0 << std::endl;
     return 0;
   }
-
-  lca(n, m, parents);
+  lca(noNodes, n, m, parents);
   return 0;
 }
